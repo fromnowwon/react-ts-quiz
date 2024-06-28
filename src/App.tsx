e06ea3a1978d1b33d31a3b322589a07606f86887
cw <fromnowwon@gmail.com>
2024-06-28 15:18:28 +0900

    style: 결과 페이지 스타일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -114,7 +114,7 @@ function App() {
 		<main className="py-10 h-screen space-y-5">
 			<h1 className="font-bold text-3xl text-center">REACT QUIZ</h1>
 
-			<div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
+			<div className="max-w-lg mx-auto bg-slate-100 rounded-md p-7">
 				{!(questions.length > 0) && !loading && !finish && (
 					<div className="text-center">
 						<div className="flex items-center justify-center">
--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,4 +1,4 @@
-import { PartyPopper } from "lucide-react";
+import { Dot, PartyPopper } from "lucide-react";
 import { UserState } from "../types/Question";
 import { filterHtml } from "../util";
 
@@ -12,21 +12,43 @@ const Result: React.FC<ResultProps> = ({ scores, userState }) => {
 
 	return (
 		<div>
-			<h2>
-				<PartyPopper />
-				결과
-				<PartyPopper />
-			</h2>
-			{userState.map((answer, idx) => (
-				<li key={idx}>
-					<p>
-						{idx + 1}번: {answer.correct ? 1 : 0}점
-					</p>
-					<p>정답: {filterHtml(answer.correctAnswer)}</p>
-					<p>내가 선택한 답: {filterHtml(answer.selectedAnswer)}</p>
-				</li>
-			))}
-			<p>총 {totalScore}점</p>
+			<h2 className="flex justify-center font-bold text-2xl">결과</h2>
+			<p className="mt-2 pb-3 text-center font-bold text-lg">
+				총 {totalScore}점
+			</p>
+
+			<ul className="flex flex-col gap-3">
+				{userState.map((answer, idx) => (
+					<li
+						key={idx}
+						className="list-none flex flex-col gap-1 border-t-2 pt-3 px-5"
+					>
+						<p
+							className={`flex font-bold ${
+								answer.correct ? "text-green-600" : "text-red-600"
+							}`}
+						>
+							{idx + 1}번 ({answer.correct ? "정답" : "오답"}
+							{answer.correct && <PartyPopper className="scale-75" />})
+						</p>
+						<p className="flex">
+							<Dot />
+							{answer.correct ? 1 : 0}점
+						</p>
+						<p className="flex">
+							<Dot />
+							선택한 답: {filterHtml(answer.selectedAnswer)}
+						</p>
+
+						{!answer.correct && (
+							<p className="flex">
+								<Dot />
+								정답: {filterHtml(answer.correctAnswer)}
+							</p>
+						)}
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
