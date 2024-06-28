0bc372221b479729818a9568a3ed9acc269cd149
cw <fromnowwon@gmail.com>
2024-06-28 16:22:47 +0900

    style: main 스타일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -123,7 +123,7 @@ function App() {
 	};
 
 	return (
-		<main className="py-10 h-screen space-y-5">
+		<main className="py-10 space-y-5">
 			<h1 className="font-bold text-3xl text-center">REACT QUIZ</h1>
 
 			<div className="max-w-lg mx-auto bg-slate-100 rounded-md p-7">
